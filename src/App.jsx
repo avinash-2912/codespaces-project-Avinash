3672dda1a14785b35c2e5d4b7d5bc2a37194940d
Avinash Kumar <105158322+avinash-2912@users.noreply.github.com>
2024-07-02 14:01:40 +0000

    customized

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Avinash Kumar",
+  title: "Full Stack Web Developer",
+  email: "avinash7797405@gmail.com.com",
+  gitHub: "github.com/avinash-2912",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/avinash-kumar-685746221/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
